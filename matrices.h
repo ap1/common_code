68f618c3848c48f11c39dd8c3ad81c034eb0327d
ap1 <anjul.patney@gmail.com>
2012-11-09 18:54:05 -0800

    more matrix functions

--- matrices.h
+++ matrices.h
@@ -110,14 +110,62 @@ inline void vtransform(const float m[16], const cvec4f& vin, cvec4f& vout){
 }
 
 inline void vtransform(const float m[16], const cvec3f& vin, cvec4f& vout){
-    cvec4f vh = gencvec4f(vin.x, vin.y, vin.z, 1.0f);
-    vtransform(m, vh, vout);
+    vout.x = vin.x * m[ 0] + vin.y * m[ 4] + vin.z * m[ 8] + m[12];
+    vout.y = vin.x * m[ 1] + vin.y * m[ 5] + vin.z * m[ 9] + m[13];
+    vout.z = vin.x * m[ 2] + vin.y * m[ 6] + vin.z * m[10] + m[14];
+    vout.w = vin.x * m[ 3] + vin.y * m[ 7] + vin.z * m[11] + m[15];
 }
 
 inline void ntransform(const float m[16], const cvec3f& vin, cvec3f& vout){
     vout.x = vin.x * m[ 0] + vin.y * m[ 1] + vin.z * m[ 2];
     vout.y = vin.x * m[ 4] + vin.y * m[ 5] + vin.z * m[ 6];
     vout.z = vin.x * m[ 8] + vin.y * m[ 9] + vin.z * m[10];
+}
+
+
+inline void mat4fSetRow(float m[16], const int row, const float c0, const float c1, const float c2, const float c3){
+  m[row*4 + 0] = c0; 
+  m[row*4 + 1] = c1; 
+  m[row*4 + 2] = c2; 
+  m[row*4 + 3] = c3;
+}
+
+inline void mat4fSetIdentity(float m[16]){
+  mat4fSetRow(m, 0, 1.0f, 0.0f, 0.0f, 0.0f);
+  mat4fSetRow(m, 1, 0.0f, 1.0f, 0.0f, 0.0f);
+  mat4fSetRow(m, 2, 0.0f, 0.0f, 1.0f, 0.0f);
+  mat4fSetRow(m, 3, 0.0f, 0.0f, 0.0f, 1.0f);
+}
+
+inline void mat4fDisplay(float m[16]){
+  dispMat(m, 16, 4);
+}
+
+inline int mat4fIdx(int r, int c){
+  return r*4 + c;
+}
+
+inline float& mat4fval(float m[16], int r, int c){
+  return m[r*4 + c];
+}
+
+inline void mat4fMult(float out[16], const float m1[16], const float m2[16]){
+  // out = m1 x m2
+  for(int r=0; r<4; r++){
+    for(int c=0; c<4; c++){
+      mat4fval(out,r,c) = 0.0f;
+      for(int k=0; k<4; k++){
+        mat4fval(out,r,c) += mat4fval(out,r,k) * mat4fval(out,k,c);
+      }
+    }
+  }
+}
+
+inline void mat4fMultInplace(float m1[16], const float m2[16]){
+  // m1 = m1 x m2
+  float tmp[16];
+  mat4fMult(tmp, m1, m2);
+  memcpy(m1, tmp, 16*sizeof(float));
 }
 
 #endif
