1af9ad46384619ee3e0c9f3cb1d1c1d8dd4895ea
ap1 <anjul.patney@gmail.com>
2012-12-10 18:04:30 -0800

    added two new macros

--- cudaInlineMisc.h
+++ cudaInlineMisc.h
@@ -3,11 +3,17 @@
 
 #include "maths.h"
 
-#define cudaKernelLaunch(kernelName, blockSize, nTotalElements,...)  do {   \
+#define cuiKernelLaunch(kernelName, blockSize, nTotalElements,...)  do {   \
                                                                          dim3 blk(blockSize,1,1); \
                                                                          dim3 grd(ceil_int_div(nTotalElements,blk.x),1,1); \
                                                                          kernelName<<<grd,blk>>>(__VA_ARGS__); \
                                                                      }while(0)
+
+
+#define cuiMallocArray(    memptr, arrcount, datatype) do {cudaMalloc((void**)memptr, arrcount*sizeof(datatype));}while(0)
+#define cuiMallocSingleton(memptr,           datatype) do {cudaMalloc((void**)memptr,           1*sizeof(datatype));}while(0)
+
+//cudaMalloc((void**)&rstbins.VSPrimCount,              1 * sizeof(int));
 
 #define GLOBAL_TID (blockIdx.x * blockDim.x + threadIdx.x)
 
