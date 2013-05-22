78eeb307cfded84a8cd101526a71d79e8348ff59
ap1 <anjul.patney@gmail.com>
2013-05-21 19:03:22 -0700

    added code for generating color palettes

--- colors.h
+++ colors.h
@@ -20,5 +20,45 @@ inline uint rgbaFloatToInt(float r, float g, float b, float a=0.0f){
     return ((uint)(a*255)<<24) | ((uint)(b*255)<<16) | ((uint)(g*255)<<8) | (uint)(r*255);
 }
 
+#define COLOR_PALETTE_GEN_RGB \
+    float r = r_beg + RAND_01 * (r_end - r_beg);  \
+    float g = g_beg + RAND_01 * (g_end - g_beg);  \
+    float b = b_beg + RAND_01 * (b_end - b_beg);  \
+\
+    r = all_beg + r * (all_end - all_beg);  \
+    g = all_beg + g * (all_end - all_beg);  \
+    b = all_beg + b * (all_end - all_beg);  
+
+
+inline uint* generate_uint_palette(
+    int   count    = 256,
+    float all_beg  = 0.0f, float all_end  = 1.0f,
+    float r_beg    = 0.0f, float r_end    = 1.0f,
+    float g_beg    = 0.0f, float g_end    = 1.0f,
+    float b_beg    = 0.0f, float b_end    = 1.0f) {
+
+  uint* outcolors = new uint[count];
+  for(int i=0; i<count; i++) {
+    COLOR_PALETTE_GEN_RGB;
+    outcolors[i] = rgbaFloatToInt(r,g,b);
+  }
+  return outcolors;
+}
+
+inline vec3f* generate_vec3f_palette(
+    int   count    = 256,
+    float all_beg  = 0.0f, float all_end  = 1.0f,
+    float r_beg    = 0.0f, float r_end    = 1.0f,
+    float g_beg    = 0.0f, float g_end    = 1.0f,
+    float b_beg    = 0.0f, float b_end    = 1.0f) {
+
+  cvec3f* outcolors = new vec3f[count];
+  for(int i=0; i<count; i++){
+    COLOR_PALETTE_GEN_RGB;
+    outcolors[i] = gencvec3f(r,g,b);
+  }
+  return outcolors;
+}
+
 
 #endif
